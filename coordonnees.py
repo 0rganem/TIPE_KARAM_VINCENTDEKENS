0420f718c00547794ef3b2b1d1760748d58f0721
0rganem <morgane.201002kens@gmail.com>
2025-06-15 17:24:45 +0200

    Update coordonnees.py

--- coordonnees.py
+++ coordonnees.py
@@ -3,20 +3,21 @@ import csv
 crd = []
 with open ('crd.csv', newline='') as fichier:
     lect = csv.reader(fichier, delimiter=",")
-    next(lect)
+    next(lect) #ne pas prendre le titre
     for lgn in lect :
-        crd.append((float(lgn[1])*1000,float(lgn[2])*1000)) #tableau de tuple (latitude,longitude)
-print(crd)
+		#tableau:latitude,longitude
+        crd.append((float(lgn[1]),float(lgn[2]))) 
+
 
 
 def est_dans_polygone(point, polygone):
-    x, y = point
+    x, y = point #coordonnées du point
     intersections = 0
     n = len(polygone)
 
     for i in range(n):
        x1, y1 = polygone[i]
-       x2, y2 = polygone[(i + 1) % n]  # Boucle sur les arêtes du polygone
+       x2, y2 = polygone[(i + 1) % n] #définie les aretes
        if y1 <= y < y2 or y2 <= y < y1:
             # Calcul de l'abscisse de l'intersection
            x_inter = x1 + (y - y1) * (x2 - x1) / (y2 - y1)
